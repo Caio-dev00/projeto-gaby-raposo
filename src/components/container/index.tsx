736fa1b13d0403fb9619d03d3369aa2aaeab848b
Caio Henrique Marques <caiohenrique1146@gmail.com>
2024-02-14 09:19:36 -0300

    Componente Container criado

--- src/components/container/index.tsx
+++ src/components/container/index.tsx
@@ -1,6 +1,9 @@
+import { ReactNode } from "react" 
 
-export function Container() {
+export function Container({children}: {children: ReactNode}) {
   return (
-    <div>Container</div>
+    <div className="w-full max-w-7xl mx-auto px-4">
+        {children}
+    </div>
   )
 }
