4d241b4f6c3dcf6a102204db33d025a241b44324
zandonadi1337 <joaozandonadidev@gmail.com>
2024-02-17 09:50:13 -0300

    adicionando cores do projeto

--- src/pages/product/index.tsx
+++ src/pages/product/index.tsx
@@ -1,7 +1,36 @@
-
+import { Container } from "../../components/container"
 
 export function ProductDetail() {
   return (
-    <div>produto</div>
+    <Container>
+      <div className="flex flex-row justify-center mt-20">
+        <div className="flex w-full h-full">
+          <div className="flex flex-col gap-2 justify-between w-24 h-24">
+            <img src="/src/assets/produto.png" alt="" className="" />
+            <img src="/src/assets/produto.png" alt="" className="" />
+            <img src="/src/assets/produto.png" alt="" className="" />
+            <img src="/src/assets/produto.png" alt="" className="" />
+          </div>
+          <img src="/src/assets/produto.png" alt="" className="ml-2 w-full h-full aspect-square object-cover" />
+          <div className="flex ml-4 w-full h-full flex-col">
+            <div className="uppercase">
+              <h1 className="font-semibold font-xl text-black">Produto Modelo</h1>
+              <h2 className="text-xl font-bold ">R$99,90</h2>
+              <hr className="my-4 w-60 "/>
+              <h3>Tamanhos</h3>
+
+
+
+
+            </div>
+
+          </div>
+
+        </div>
+
+
+      </div>
+    </Container>
+
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,8 +7,12 @@ export default {
   theme: {
     extend: {
       colors: {
-        'salmon-light': '#F5D8DA'
-      }
+        'salmon-light': '#F5D8DA',
+        'vinho-principal': '#A66576',
+        'gray-princpial': '#505050',
+        'gray-white-1': '#d9d9d9',
+        'gray-white-2': '#f5f5f5'
+
     },
   },
   plugins: [],
