11f9b3ac284a6d212d9a9c48cb1aa50f36e2f862
zandonadi1337 <joaozandonadidev@gmail.com>
2024-02-19 10:41:42 -0300

    alterações alguns códigos

--- src/pages/product/index.tsx
+++ src/pages/product/index.tsx
@@ -15,22 +15,22 @@ export function ProductDetail() {
         </div>
         <div className="flex ml-10 h-full">
             <div className="uppercase">
-              <h1 className="font-semibold text-[20px] text-gray-princpial">Produto Modelo</h1>
+              <h1 className="font-semibold text-[20px] text-gray-500">Produto Modelo</h1>
               <h2 className="text-[32px] font-bold text-vinho-principal">R$99,90</h2>
               <hr className="mt-[25px] w-60" />
               <h3 className="font-bold mt-[25px]">Tamanhos</h3>
               <div className="flex flex-row gap-2 uppercase font-semibold">
-                <button className="w-8 h-8 rounded-full mt-2 ml-2 bg-salmon-light ">P</button>
-                <button className="w-8 h-8 rounded-full mt-2 ml-2 bg-salmon-light ">M</button>
-                <button className="w-8 h-8 rounded-full mt-2 ml-2 bg-salmon-light ">G</button>
-                <button className="w-8 h-8 rounded-full mt-2 ml-2 bg-salmon-light ">GG</button>
+                <button className="w-8 h-8 rounded-full mt-2 ml-2 bg-salmon">P</button>
+                <button className="w-8 h-8 rounded-full mt-2 ml-2 bg-salmon">M</button>
+                <button className="w-8 h-8 rounded-full mt-2 ml-2 bg-salmon">G</button>
+                <button className="w-8 h-8 rounded-full mt-2 ml-2 bg-salmon">GG</button>
               </div>
               <h4 className="font-bold mt-[25px] ">Cores</h4>
               <div className="flex flex-row gap-2">
-                <button className="w-8 h-8 rounded-full mt-2 ml-2 shadow shadow-gray-600 bg-white"></button>
-                <button className="w-8 h-8 rounded-full mt-2 ml-2 shadow shadow-gray-600 bg-red-600"></button>
-                <button className="w-8 h-8 rounded-full mt-2 ml-2 shadow shadow-gray-600 bg-cyan-500"></button>
-                <button className="w-8 h-8 rounded-full mt-2 ml-2 shadow shadow-gray-600 bg-rose-600"></button>
+                <button className="w-8 h-8 rounded-full mt-2 ml-2 shadow shadow-gray-500 bg-white"></button>
+                <button className="w-8 h-8 rounded-full mt-2 ml-2 shadow shadow-gray-500 bg-red-600"></button>
+                <button className="w-8 h-8 rounded-full mt-2 ml-2 shadow shadow-gray-500 bg-cyan-500"></button>
+                <button className="w-8 h-8 rounded-full mt-2 ml-2 shadow shadow-gray-500 bg-rose-600"></button>
               </div>
               <h5 className="font-bold mt-[25px]">Quantidade</h5>
               <div className="flex flex-row mt-2 ml-2 w-[80px] h-[30px] justify-around rounded border-solid border-2 border-black">
