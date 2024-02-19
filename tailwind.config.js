1b4d56e7e7f7c02ce2405fdfb8fc5aedfbe533a3
Caio Henrique Marques <caiohenrique1146@gmail.com>
2024-02-19 10:33:35 -0300

    tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,27 +5,15 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-<<<<<<< HEAD
       extend: {
         colors: {
          'salmon': "#F5D8DA",
          'gray-500': '#505050',
          'wine-light': '#A66576',
          'wine-black': '#995A6A',
+         'gray-white-1': '#d9d9d9',
+        'gray-white-2': '#f5f5f5',
         }
       },    
     }
-=======
-    extend: {
-      colors: {
-        'salmon-light': '#F5D8DA',
-        'vinho-principal': '#A66576',
-        'gray-princpial': '#505050',
-        'gray-white-1': '#d9d9d9',
-        'gray-white-2': '#f5f5f5',
-    },
-  },
-  plugins: [],
->>>>>>> page-register
-}
 }
