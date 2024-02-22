faa20578f828ddf8556fac12352fcae0962b62ef
zandonadi1337 <joaozandonadidev@gmail.com>
2024-02-22 15:04:09 -0300

    adicionando input para observações do pedido e botão adicionar ao carrinho na pagina do produto

--- src/pages/product/index.tsx
+++ src/pages/product/index.tsx
@@ -14,7 +14,7 @@ export function ProductDetail() {
           <img src="/src/assets/produto.png" alt="" className=" h-full w-full" />
         </div>
         <div className="flex ml-10 h-full">
-            <div className="uppercase">
+            <div className="uppercase w-full">
               <h1 className="font-semibold text-[20px] text-gray-500">Produto Modelo</h1>
               <h2 className="text-[32px] font-bold text-vinho-principal">R$99,90</h2>
               <hr className="mt-[25px] w-60" />
@@ -33,10 +33,16 @@ export function ProductDetail() {
                 <button className="w-8 h-8 rounded-full mt-2 ml-2 shadow shadow-gray-500 bg-rose-600"></button>
               </div>
               <h5 className="font-bold mt-[25px]">Quantidade</h5>
-              <div className="flex flex-row mt-2 ml-2 w-[80px] h-[30px] justify-around rounded border-solid border-2 border-black">
+              <div className="flex flex-row mt-2 ml-2 w-[90px] h-[30px] justify-around rounded-[12px] border-solid border-2 border-black">
                 <button className="font-bold">-</button>
                 <button className="font-bold">1</button>
                 <button className="font-bold">+</button>
+              </div>
+              <div className="flex mt-[25px]">
+                <input className="w-full h-[80px] text-[15px] border-2 rounded-lg border-gray-500 border-opacity-50" placeholder="Observações do Pedido" type="text"/>
+              </div>
+              <div className="mt-[25px]">
+                <button className="uppercase flex flex-row w-full justify-center items-center font-semibold text-white text-[14px] rounded-md h-[32px] bg-wine-black">Adicionar ao Carrinho</button>
               </div>
             </div>
         </div>
