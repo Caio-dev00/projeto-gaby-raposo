c0d98dd08691b2b156468ce495ef7fbf9374319b
Caio Henrique Marques <caiohenrique1146@gmail.com>
2024-03-04 15:53:26 -0300

    Começando estilização dinamica do dashboard

---
+++ src/components/titleDahsboard/index.tsx
@@ -0,0 +1,15 @@
+import { ReactNode } from 'react'
+
+interface TitleProps {
+    children: ReactNode,
+    name: string
+}
+
+export default function Title({children, name}: TitleProps ) {
+  return (
+    <div className='flex flex-row items-center my-4 rounded-[4px] p-2 bg-wine-black drop-shadow-md'>
+        {children}
+        <span className='text-white font-bold text-[1.2rem] ml-4'>{name}</span>
+    </div>
+  )
+}
--- src/pages/dashboard/new/index.tsx
+++ src/pages/dashboard/new/index.tsx
@@ -1,6 +1,21 @@
+import { HeaderDashboard } from "../../../components/headerDashboard";
+import Title from "../../../components/titleDahsboard";
+
+import { FaListAlt } from "react-icons/fa";
+
 
 export function New() {
   return (
-    <div></div>
+    <div>
+      <HeaderDashboard/>
+
+      
+      <div className="ml-[300px] pt-[1px] px-[16px]">
+          <Title name="Lista de Produtos">
+            <FaListAlt size={25} color="#FFF" />
+          </Title>
+        </div>
+
+    </div>
   )
 }
