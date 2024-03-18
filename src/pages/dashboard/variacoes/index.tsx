9c6a7856ecdf3f60d6fb2cd2a577695b1f4059bf
zandonadi1337 <joaozandonadidev@gmail.com>
2024-03-18 09:52:09 -0300

    finalização opção cadastro variação tamanho

--- src/pages/dashboard/variacoes/index.tsx
+++ src/pages/dashboard/variacoes/index.tsx
@@ -2,8 +2,48 @@ import { HeaderDashboard } from "../../../components/headerDashboard";
 import Title from "../../../components/titleDahsboard";
 import { FaEdit } from "react-icons/fa";
 import { Link } from "react-router-dom";
+import { useState, useContext, useEffect } from "react";
+import { AuthContext } from "../../../contexts/AuthContext";
+import { collection, doc, getDocs, orderBy, query } from "firebase/firestore";
+import { db } from "../../../services/firebaseConnection";
+import { FaTrashCan } from "react-icons/fa6";
+import { FiEdit2 } from "react-icons/fi";
+
+interface tamanhoProps {
+    uid: string,
+    name: string,
+    owner: string,
+}
+
 
 export function Variacoes() {
+
+    const { user } = useContext(AuthContext)
+    const [tamanho, setTamanho] = useState<tamanhoProps[]>([])
+
+    useEffect(() => {
+        loadTamanhos()
+
+    }, [])
+
+    async function loadTamanhos() {
+        const tamanhosRef = collection(db, "Tamanho")
+        const q = query(tamanhosRef, orderBy("created", "desc"))
+
+        await getDocs(q)
+            .then((snapshot) => {
+                const listaTamanhos = [] as tamanhoProps[]
+                snapshot.forEach(doc => {
+                    listaTamanhos.push({
+                        uid: doc.id,
+                        name: doc.data().tamanho,
+                        owner: doc.data().owner
+                    })
+                    setTamanho(listaTamanhos)
+                })
+            }
+            )
+    }
     return (
         <div>
             <HeaderDashboard />
@@ -18,7 +58,7 @@ export function Variacoes() {
                         <Link to="/dashboard/variacoes/variacoes-tamanho">
                             <span className="text-white text-[11px] font-semibold">CADASTRAR TAMANHO</span>
                         </Link>
-                    </button> 
+                    </button>
 
                     <button className="flex justify-center items-center w-36 h-10 float-right bg-wine-black rounded-full hover:scale-105 duration-300 sm:w-48 lg:w-56">
                         <Link to="#">
@@ -26,7 +66,7 @@ export function Variacoes() {
                         </Link>
                     </button>
                 </div>
-                
+
                 <div>
                     <table className="w-full text-center border-solid border my-2 p-0 table-fixed border-collapse max-sm:border-0">
                         <thead className="max-sm:border-none max-sm:m-[-1px] max-sm:h-[1px] max-sm:overflow-hidden max-sm:p-0 max-sm:w-[1px]">
@@ -36,6 +76,27 @@ export function Variacoes() {
                                 <th scope="col">Ações</th>
                             </tr>
                         </thead>
+                        {tamanho.map(item => (
+                            <tbody key={item.uid}>
+                            <tr className="bg-white border border-solid text-[14px] border-zinc-300 max-sm:text-[12px] max-sm:p-1">
+                                <td className="border-0 rounded-[4px] py-2" data-label="nome">{item.name}</td>
+                                <td className="border-0 rounded-[4px] py-2" data-label="tipo da variacao">{item.uid}</td>
+
+                                <td className="border-0 rounded-[4px] py-2" data-label="ações">
+                                    <button>
+                                        <div className="flex gap-3">
+                                            <Link to={`/dashboard/new:id`}>
+                                                <FiEdit2 size={17} color="#000" />
+                                            </Link>
+                                            <button>
+                                                <FaTrashCan size={17} color="#000" />
+                                            </button>
+                                        </div>
+                                    </button>
+                                </td>
+                            </tr>
+                        </tbody>
+                        ))}
                     </table>
                 </div>
 
