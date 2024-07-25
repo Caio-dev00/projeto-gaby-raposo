fac0b6fdcc4f6063b8e6a9a243097fedc81b16a3
Caio Henrique Marques <caiohenrique1146@gmail.com>
2024-07-25 09:25:28 -0300

    arquivo .toml adicionado

--- src/App.tsx
+++ src/App.tsx
@@ -1,107 +1,54 @@
-import { createBrowserRouter } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import { Home } from "./pages/home";
 import { ProductDetail } from "./pages/product";
 import { Dashboard } from "./pages/dashboard";
 import { New } from "./pages/dashboard/new";
-import { Categorias } from "./pages/dashboard/categorias"
-import { Login } from "./pages/login"; 
+import { Categorias } from "./pages/dashboard/categorias";
+import { Login } from "./pages/login";
 import { Register } from "./pages/register";
 import { CadastrarCategoria } from "./pages/dashboard/categorias/cadastrar-categoria";
 import { Variacoes } from "./pages/dashboard/variacoes";
-import { CadastrarTamanho } from "./pages/dashboard/variacoes/variacoes-tamanho";
-import { CadastrarCor } from "./pages/dashboard/variacoes/variacoes-cor";
 import { Banners } from "./pages/dashboard/banners";
 import { CadastrarBanner } from "./pages/dashboard/banners/cadastrar-banner";
 
 import { Layout } from "./components/layout";
-import { Private } from "./routes/Private";
 import Produtos from "./pages/produtos/produtos";
+import ProtectedRegister from "./pages/register/RegisterProtected";
+import { Private } from "./routes/Private";
+import { CadastrarTamanho } from "./pages/dashboard/variacoes/variacoes-tamanho";
+import { CadastrarCor } from "./pages/dashboard/variacoes/variacoes-cor";
 
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="product/:id" element={<ProductDetail />} />
+          <Route path="produtos/:categoria/:tamanho" element={<Produtos />} />
+          <Route path="login" element={<Login />} />
+          <Route path="adm-2024" element={<ProtectedRegister />} />
+          <Route path="adm-2024/admin" element={<Register />} />
+        </Route>
 
-const router = createBrowserRouter([
-  {
-    element: <Layout />,
-    children: [
-      {
-        path: "/",
-        element: <Home />
-      },
-      {
-        path: "/product/:id",
-        element: <ProductDetail />
-      },
-      {
-        path: "/produtos/:categoria/:tamanho",
-        element: <Produtos />
-      },
-      
-    ]
-  },
-  {
-    path: "/login",
-    element: <Login />
-  },
-  {
-    path: "/adm-2024",
-    element: <Register />
-  },
-  {
-    path: "/dashboard",
-    element: <Private><Dashboard /></Private>
-  },
-  {
-    path: "/dashboard/new",
-    element: <Private><New /></Private>
-  },
-  {
-    path: "/dashboard/new/:id",
-    element: <Private><New /></Private>
-  },
-  {
-    path: "/dashboard/categorias",
-    element: <Private><Categorias /></Private>
-  },
-  {
-    path: "/dashboard/categorias/cadastrar-categoria",
-    element: <Private><CadastrarCategoria /></Private>
-  },
-  {
-    path: "/dashboard/categorias/cadastrar-categoria/:id",
-    element: <Private><CadastrarCategoria /></Private>
-  },
-  {
-    path: "/dashboard/variacoes",
-    element: <Private><Variacoes/></Private>
-  },
-  {
-    path: "/dashboard/variacoes/variacoes-tamanho",
-    element: <Private><CadastrarTamanho/></Private>
-  },
-  {
-    path: "/dashboard/variacoes/variacoes-tamanho/:id",
-    element: <Private><CadastrarTamanho/></Private>
-  },
-  {
-    path: "/dashboard/variacoes/variacoes-cor",
-    element: <Private><CadastrarCor/></Private>
-  },
-  {
-    path: "/dashboard/variacoes/variacoes-cor/:id",
-    element: <Private><CadastrarCor/></Private>
-  },
-  {
-    path: "/dashboard/banners",
-    element: <Private><Banners/></Private>
-  },
-  {
-    path: "/dashboard/banners/cadastrar-banner",
-    element: <Private><CadastrarBanner/></Private>
-  },
-  {
-    path:  "/dashboard/banners/cadastrar-banner/:id",
-    element: <Private><CadastrarBanner/></Private>
-  },
-])
+        <Route path="/dashboard" element={<Private><Dashboard /></Private>} />
+        <Route path="/dashboard/new" element={<Private><New /></Private>} />
+        <Route path="/dashboard/new/:id" element={<Private><New /></Private>} />
+        <Route path="/dashboard/categorias" element={<Private><Categorias /></Private>} />
+        <Route path="/dashboard/categorias/cadastrar-categoria" element={<Private><CadastrarCategoria /></Private>} />
+        <Route path="/dashboard/categorias/cadastrar-categoria/:id" element={<Private><CadastrarCategoria /></Private>} />
+        <Route path="/dashboard/variacoes" element={<Private><Variacoes /></Private>} />
+        <Route path="/dashboard/variacoes/variacoes-tamanho" element={<Private><CadastrarTamanho /></Private>} />
+        <Route path="/dashboard/variacoes/variacoes-tamanho/:id" element={<Private><CadastrarTamanho /></Private>} />
+        <Route path="/dashboard/variacoes/variacoes-cor" element={<Private><CadastrarCor /></Private>} />
+        <Route path="/dashboard/variacoes/variacoes-cor/:id" element={<Private><CadastrarCor /></Private>} />
+        <Route path="/dashboard/banners" element={<Private><Banners /></Private>} />
+        <Route path="/dashboard/banners/cadastrar-banner" element={<Private><CadastrarBanner /></Private>} />
+        <Route path="/dashboard/banners/cadastrar-banner/:id" element={<Private><CadastrarBanner /></Private>} />
+      </Routes>
+    </Router>
+  );
+}
 
-export { router }
+export default App;
